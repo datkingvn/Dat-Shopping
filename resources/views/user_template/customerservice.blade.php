777e7c4c7022243c119ba52d1a25eddcc2d1a346
Mạnh Đạt <tranhuudats1@gmail.com>
2023-08-18 09:43:23 +0700

    update User Interface

---
+++ resources/views/user_template/customerservice.blade.php
@@ -0,0 +1,4 @@
+@extends('user_template.layouts.template')
+@section('main-content')
+    <h2>Customer service</h2>
+@endsection
---
+++ resources/views/user_template/newrelease.blade.php
@@ -0,0 +1,4 @@
+@extends('user_template.layouts.template')
+@section('main-content')
+    <h2>New Realease</h2>
+@endsection
